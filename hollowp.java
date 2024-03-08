989cc4ba9ae6973922c5a42acc9180f124906761
sagar-prajapat <154601734+sagar-prajapat@users.noreply.github.com>
2024-03-08 16:58:59 +0530

    Add files via upload

---
+++ hollowp.java
@@ -0,0 +1,19 @@
+public class hollowp {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*" + " ");
+                } else if ((i + j) % 2 == 0) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print(" " + " ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+
+}
